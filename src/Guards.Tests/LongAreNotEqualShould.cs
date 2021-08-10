ad278ddad56f0eac8ed0b29f17a9f91e8f4635f3
Andras Csanyi <andras.csanyi@pm.me>
2021-08-10 20:19:34 +0200

    coverage enhancement

--- src/Guards.Tests/LongAreNotEqualShould.cs
+++ src/Guards.Tests/LongAreNotEqualShould.cs
@@ -21,5 +21,46 @@ namespace EncyclopediaGalactica.Guards.Tests
             action.Should().ThrowExactly<GuardException>();
         }
         
+        [Fact]
+        public void DoesntThrow_WhenValuesArentEqual()
+        {
+            // Arrange
+            long one = 1;
+            long two = 2;
+
+            // Act
+            Action action = () => { Check.AreNotEqual(one, two); };
+
+            // Assert
+            action.Should().NotThrow<GuardException>();
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenValuesAreEqual_LongIsNullable()
+        {
+            // Arrange
+            long? one = 1;
+            long one_not_nullable = 1;
+            
+            // Act
+            Action action = () => { Check.AreNotEqual(one, one_not_nullable); };
+            
+            // Assert
+            action.Should().ThrowExactly<GuardException>();
+        }
+        
+        [Fact]
+        public void DoesntThrowException_WhenValuesAreEqual_LongIsNullable()
+        {
+            // Arrange
+            long? one = 1;
+            long one_not_nullable = 2;
+            
+            // Act
+            Action action = () => { Check.AreNotEqual(one, one_not_nullable); };
+            
+            // Assert
+            action.Should().NotThrow<GuardException>();
+        }
     }
 }
